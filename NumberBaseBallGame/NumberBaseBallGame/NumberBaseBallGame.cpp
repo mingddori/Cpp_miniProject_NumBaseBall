5925307a3723affe7e9cf02e573fd35e57a3459f
arominddo <godhsb@naver.com>
2022-12-01 16:44:16 +0900

    2022.12.01.(1)

---
+++ NumberBaseBallGame/NumberBaseBallGame/NumberBaseBallGame.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include "stdio.h"
+
+void startScene();
+
+int main()
+{
+    // 게임 시작 화면
+    startScene();
+
+    //getch();
+    // 메뉴 선택
+
+    //
+    
+}
+
+void startScene() {
+    std::cout << "  ____________________________________________________________________________________________________ " << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |             ####                          ##                         ##                           |" << std::endl;
+    std::cout << "  |           ##    ##         ############   ##            ######       ##        ############       |" << std::endl;
+    std::cout << "  |         ##        ##            ##        ##          ##      ##     ##                  ##       |" << std::endl;
+    std::cout << "  |                                ####       ##        ##          ##   ####                ##       |" << std::endl;
+    std::cout << "  |        ##############         ##  ##      ####      ##          ##   ##                  ##       |" << std::endl;
+    std::cout << "  |              ##              ##    ##     ##        ##          ##   ####                         |" << std::endl;
+    std::cout << "  |                             ##      ##    ##          ##      ##     ##        #############      |" << std::endl;
+    std::cout << "  |             ####           ##        ##   ##            ######       ##             ##            |" << std::endl;
+    std::cout << "  |           ##    ##                        ##                         ##             ##            |" << std::endl;
+    std::cout << "  |         ##        ##                                                                              |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |===================================================================================================|" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                     1. 혼자 게임하기                                              |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                     2. 둘이 게임하기                                              |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                     3. 제작자                                                     |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                     4. 종료하기                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                                                   |" << std::endl;
+    std::cout << "  |                                                                        made by. Lee MinSeop       |" << std::endl;
+    std::cout << "  |___________________________________________________________________________________________________|" << std::endl;
+
+      
+
+    
+
+
+   
+
+
+}
---
+++ NumberBaseBallGame/NumberBaseBallGame/작성 유용 코드.cpp
@@ -0,0 +1,26 @@
+/*
+void main()
+{
+    int a;
+    while(1) {
+        printf("숫자입력(1:초기화, 2: 종료) : ");
+        scanf("%d", &a);
+        if(a==1) {
+            system("cls");
+        } else if(a==2) {
+            break;
+        }
+    }
+}
+*/
+
+/*
+int main()
+{
+    printf("지우기 1초전\n");
+    Sleep(1000);   //1초 딜레이
+    system("cls"); //콘솔화면 지우기
+    Sleep(1000);
+    printf("지우기 완료\n");
+}
+*/
